966f23f71abb1d900c4ba45ce2302974086eaf63
JulyAn1234 <l20211790@tectijuana.edu.mx>
2024-07-28 21:30:49 -0700

    Init Project and added a health check

---
+++ src/main/java/com/ecommerce_auth/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.ecommerce_auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+    @GetMapping
+    public String HealthCheck() {
+        return "Application Running correctly";
+    }
+}
